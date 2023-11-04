8e8a8e8acd4bc009eebf340a6cb9f9b3c70285f1
Satyajit Sahoo <satyajit.happy@gmail.com>
2023-11-04 21:06:29 +0100

    Fix deployment

--- next.config.js
+++ next.config.js
@@ -3,21 +3,9 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx',
 });
 
-let assetPrefix = '';
-let basePath = '';
-
-if (process.env.GITHUB_ACTIONS) {
-  const repo = 'react-native-builder-bob';
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 module.exports = withNextra({
   output: 'export',
   images: {
     unoptimized: true,
   },
-  assetPrefix,
-  basePath,
 });
