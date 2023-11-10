cc9fd0ca5686aba9a1af79a673ce6dfce36f433b
satya164 <satyajit.happy@gmail.com>
2023-11-10 14:46:31 +0000

    Deploying to gh-pages from @ satya164/satya164.github.io@71df459a9933fb3cb106d4b6d1bc08c84459a38b 🚀

---
+++ _next/static/chunks/app/posts/layout-f4f78047656e16bb.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[517],{4083:function(e,n,t){Promise.resolve().then(t.bind(t,9670))},9670:function(e,n,t){"use strict";t.r(n),t.d(n,{SocialLinks:function(){return SocialLinks}});var r=t(7437),o=t(4033),c=t(2265),s=t(102),a=t.n(s);function SocialLinks(){let[e,n]=(0,c.useState)(""),t=(0,o.usePathname)();(0,c.useEffect)(()=>{n(document.title)},[]);let s="".concat("https://satya164.page").concat(t);return(0,r.jsxs)("ul",{className:a().social,children:[(0,r.jsx)("li",{children:(0,r.jsx)("a",{href:"https://x.com/intent/tweet?text=".concat(encodeURIComponent(e),"&url=").concat(encodeURIComponent(s),"&via=satya164"),target:"_blank",rel:"noopener noreferrer",children:"Share on X"})}),(0,r.jsx)("li",{children:(0,r.jsx)("a",{href:"https://twitter.com/search?q=".concat(encodeURIComponent(s)),target:"_blank",rel:"noopener noreferrer",children:"Discuss on X"})})]})}},102:function(e){e.exports={social:"styles_social__ZeynR"}},622:function(e,n,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var r=t(2265),o=Symbol.for("react.element"),c=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),s=r.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,a={key:!0,ref:!0,__self:!0,__source:!0};function q(e,n,t){var r,i={},l=null,u=null;for(r in void 0!==t&&(l=""+t),void 0!==n.key&&(l=""+n.key),void 0!==n.ref&&(u=n.ref),n)c.call(n,r)&&!a.hasOwnProperty(r)&&(i[r]=n[r]);if(e&&e.defaultProps)for(r in n=e.defaultProps)void 0===i[r]&&(i[r]=n[r]);return{$$typeof:o,type:e,key:l,ref:u,props:i,_owner:s.current}}n.jsx=q,n.jsxs=q},7437:function(e,n,t){"use strict";e.exports=t(622)},4033:function(e,n,t){e.exports=t(94)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=4083)}),_N_E=e.O()}]);
