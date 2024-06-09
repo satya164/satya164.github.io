df31349b66b485477b7bccc572c4b91684f110e2
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-06-10 00:18:18 +0200

    Rework nav bar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,33 +1,63 @@
 'use client';
 
 import clsx from 'clsx';
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import avatar from '../../assets/images/avatar-small.jpg';
 import { ThemeSwitcher } from '../ThemeSwitcher';
 import styles from './styles.module.css';
-
-const links = [
-  { href: '/', label: 'Posts' },
-  { href: '/about', label: 'About' },
-  { href: '/rss.xml', label: 'RSS' },
-] as const;
 
 export function NavBar() {
   const pathname = usePathname();
 
   return (
     <nav className={styles.container}>
-      {links.map(({ href, label }) => {
-        return (
-          <Link
-            key={href}
-            href={href}
-            className={clsx(styles.item, pathname === href && styles.active)}
+      {pathname === '/' ? (
+        <Link href="/about" className={clsx(styles.logo, styles.item)}>
+          <Image src={avatar} alt="About" />
+        </Link>
+      ) : (
+        <Link
+          href="/"
+          className={clsx(styles.item, styles.back)}
+          aria-label="Posts"
+        >
+          <svg
+            role="img"
+            className={styles.icon}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            {label}
-          </Link>
-        );
-      })}
+            <path
+              d="M19 12H5M5 12L12 19M5 12L12 5"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </Link>
+      )}
+      <div className={styles.spacer} />
+      <Link href="/rss.xml" className={styles.item}>
+        <svg
+          className={styles.icon}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M4 11a9 9 0 0 1 9 9" />
+          <path d="M4 4a16 16 0 0 1 16 16" />
+          <circle cx="5" cy="19" r="1" />
+        </svg>
+      </Link>
       <ThemeSwitcher className={styles.item} />
     </nav>
   );
