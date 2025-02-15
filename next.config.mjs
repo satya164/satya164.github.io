d698c1976839eab4ce4fb04ef3429c3238f0c20a
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-02-15 17:35:23 +0100

    Support video tags in markdown

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 import createMDX from '@next/mdx';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypeMdxImportMedia from 'rehype-mdx-import-media';
 import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeSlug from 'rehype-slug';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
 import remarkGithubAdmonitions from 'remark-github-beta-blockquote-admonitions';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
-import remarkMdxImages from 'remark-mdx-images';
 import { remarkMdxToc } from 'remark-mdx-toc';
 import readingTime from 'remark-reading-time';
 import readingTimeMdx from 'remark-reading-time/mdx.js';
+import { visit } from 'unist-util-visit';
 import rehypeCodeblockMeta from './src/plugins/rehype-codeblock-meta.mjs';
 
 /** @type {import('next').NextConfig} */
@@ -22,6 +23,23 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   experimental: {
     typedRoutes: true,
+  },
+  webpack: (webpackConfig, { dev, isServer }) => {
+    webpackConfig.module.rules.push({
+      test: /\.mp4$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/media/',
+            outputPath: `${dev ? '' : '../'}${isServer ? '../' : ''}static/media/`,
+            name: '[name].[hash:8].[ext]',
+          },
+        },
+      ],
+    });
+
+    return webpackConfig;
   },
 };
 
@@ -36,7 +54,6 @@ const withMDX = createMDX({
     format: 'mdx',
     remarkPlugins: [
       remarkGfm,
-      remarkMdxImages,
       remarkFrontmatter,
       remarkMdxFrontmatter,
       remarkMdxToc,
@@ -62,8 +79,35 @@ const withMDX = createMDX({
           },
         },
       ],
+      () => (tree) => {
+        // Replace MDX video elements with native video elements
+        // This is necessary so that rehype-mdx-import-media can handle them
+        visit(tree, 'mdxJsxFlowElement', (node) => {
+          if (
+            node.name === 'video' &&
+            node.attributes.find(
+              (attr) => attr.name === 'src' && attr.value.endsWith('.mp4')
+            )
+          ) {
+            node.type = 'element';
+            node.data = {
+              hName: 'video',
+              hProperties: Object.fromEntries(
+                node.attributes.map((attr) => [
+                  attr.name,
+                  attr.value === null ? true : attr.value,
+                ])
+              ),
+            };
+
+            delete node.attributes;
+            delete node.children;
+          }
+        });
+      },
     ],
     rehypePlugins: [
+      rehypeMdxImportMedia,
       [rehypeCodeblockMeta, { match: { playground: true } }],
       [rehypeExternalLinks, { target: '_blank' }],
       rehypeSlug,
